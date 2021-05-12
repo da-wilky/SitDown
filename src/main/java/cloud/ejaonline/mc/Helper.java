470da02fe8a0d4e9907c4ad14759cb5d5b92c4bf
Geilmaker <samuel.wilk.00@gmail.com>
2021-05-13 01:46:10 +0200

    Using world.spawn instead of world.spawnEntity and minor bugfixes

--- src/main/java/cloud/ejaonline/mc/Helper.java
+++ src/main/java/cloud/ejaonline/mc/Helper.java
@@ -4,18 +4,22 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Helper {
     private JavaPlugin plugin;
-    private FileConfiguration config;
+    private String vehicleMessage;
+    private String denyMessage;
+    private String sitMessage;
 
-    Helper(JavaPlugin plugin, FileConfiguration config) {
+    public Helper(JavaPlugin plugin, FileConfiguration config) {
+        // this.config = config;
         this.plugin = plugin;
-        this.config = config;
+        vehicleMessage = transStr(config.getString("vehicle-air-message"));
+        denyMessage = transStr(config.getString("missing-permission-message"));
+        sitMessage = transStr(config.getString("sit-message"));
     }
 
     public String transStr(String str) {
@@ -25,10 +29,6 @@ public class Helper {
     public void print(String str) {
         plugin.getLogger().info(str);
     };
-
-    String vehicleMessage = transStr(config.getString("vehicle-air-message"));
-    String denyMessage = transStr(config.getString("missing-permission-message"));
-    String sitMessage = transStr(config.getString("sit-message"));
 
     public void sitDown(Player p, Location location) {
         if (!p.hasPermission(SitDown.sitPermission)) {
@@ -41,13 +41,12 @@ public class Helper {
             return;
         }
 
-        ArmorStand chair = (ArmorStand) location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
-        chair.setMetadata("chair", new FixedMetadataValue(plugin, "chair"));
-
-        chair.setGravity(false);
-        chair.setVisible(false);
-
-        chair.addPassenger(p);
+        location.getWorld().spawn(location, ArmorStand.class, armorStand -> {
+            armorStand.setGravity(false);
+            armorStand.setVisible(false);
+            armorStand.setMetadata("chair", new FixedMetadataValue(plugin, "chair"));
+            armorStand.addPassenger(p);
+        });
         p.sendMessage(sitMessage);
     };
 }
